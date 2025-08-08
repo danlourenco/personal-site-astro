daa3e16c8f245f43ffdb09fbd4b71d276ac53f35
Dan Lourenco <danlourenco@gmail.com>
2025-08-08 16:55:12 -0400

    remove tracking of .astro/ directory

--- .astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/articles/rewriting-the-cosmos-kernel-in-rust.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Frewriting-the-cosmos-kernel-in-rust.mdx&astroContentModuleFlag=true")],
-["src/content/articles/ai-assisted-virtual-art-gallery-part-one.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2Fai-assisted-virtual-art-gallery-part-one.mdx&astroContentModuleFlag=true")]]);
-		
